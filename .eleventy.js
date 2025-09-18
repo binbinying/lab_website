89c53bd4db248287771e0b4444cdce50e485ab73
Zhen Yang <zhenyang@Zhens-MacBook-Pro.local>
2025-09-18 17:12:01 -0400

    hero

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,17 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/documents");
-  eleventyConfig.addPassthroughCopy("src/styles");
   eleventyConfig.addPassthroughCopy("src/images");
 
+
   return {
-    pathPrefix: process.env.ELEVENTY_PATH_PREFIX || "/lab_website/",
+    // pathPrefix: process.env.ELEVENTY_PATH_PREFIX || "/lab_website/",
+    pathPrefix: process.env.ELEVENTY_PATH_PREFIX || "/",
     dir: {
       input: "src",
       output: "_site",
       includes: "_includes"
     },
-    templateFormats: ["md", "njk"],
+    templateFormats: ["md", "njk", "css"],
     markdownTemplateEngine: "njk"
   };
 };

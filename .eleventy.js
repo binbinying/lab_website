81e0b22ed6d68a01ead6dc261ef54e21c0d358cf
Zhen Yang <zhenyang@dhcp-10-29-217-186.dyn.MIT.EDU>
2025-09-19 18:09:20 -0400

    update website

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/documents");
   eleventyConfig.addPassthroughCopy("src/images");
-
+  eleventyConfig.addGlobalData("year", () => new Date().getFullYear());
 
   return {
     // pathPrefix: process.env.ELEVENTY_PATH_PREFIX || "/lab_website/",
--- src/index.md
+++
@@ -1,9 +0,0 @@
----
-title: Welcome
-layout: layout.njk
-heroTitle: Welcome to Ying Lab!
----
-
-# Welcome to Ying Lab 🧪
-
-We build soft mucosa-interfacing medical devices.
